527540b6b55f040497c51dfa4ed1c9f72b30eb40
LittleCube <littlecubehax@gmail.com>
2024-11-13 14:57:17 -0500

    add test for printing float vars

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash -c "cd $1 && cp ../SWFModernRuntime/build/Release/SWFModernRuntime.lib ./ && ../SWFRecomp test.swf > /dev/null && mkdir -p build && cd build && cmake .. > /dev/null 2> /dev/null && cmake --build . --config Release > /dev/null" || exit
+bash -c "cd $1 && cp ../SWFModernRuntime/SWFModernRuntime.lib ./ && ../SWFRecomp test.swf > /dev/null && mkdir -p build && cd build && cmake .. > /dev/null 2> /dev/null && cmake --build . --config Release > /dev/null" || exit
 result=$(bash -c "cd $1/build && ./Release/TestSWFRecompiled")
 
 echo ""
