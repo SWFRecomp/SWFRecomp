0b40c9c8a1c3c81f943203ca066b605773abe26f
LittleCube <littlecubehax@gmail.com>
2025-07-28 17:34:14 -0400

    draw lines

--- include/swf.hpp
+++ include/swf.hpp
@@ -39,6 +39,7 @@ namespace SWFRecomp
 	{
 		std::vector<Vertex> verts;
 		u32 fill_styles[2];
+		u32 line_style;
 		bool self_closed;
 		bool backward;
 		Node* front;
@@ -69,6 +70,14 @@ namespace SWFRecomp
 	struct FillStyle
 	{
 		u8 type;
+		u8 r;
+		u8 g;
+		u8 b;
+	};
+	
+	struct LineStyle
+	{
+		u16 width;
 		u8 r;
 		u8 g;
 		u8 b;
@@ -118,5 +127,6 @@ namespace SWFRecomp
 		void constructEdges(std::vector<Path>& paths, std::vector<Node>& nodes);
 		void johnson(std::vector<Node>& nodes, std::vector<Path>& path_stack, std::unordered_map<Node*, bool>& blocked, std::unordered_map<Node*, std::vector<Node*>>& blocked_map, std::vector<std::vector<Path>>& closed_paths);
 		void fillShape(std::vector<Vertex>& shape, std::vector<Tri>& tris, bool fill_right);
+		void drawLines(const Path& path, u16 width, std::vector<Tri>& tris);
 	};
 };
