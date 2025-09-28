035e45873acc13807cd1afee7b4c4879a496abc3
LittleCube <littlecubehax@gmail.com>
2025-09-27 23:29:56 -0400

    add radial gradient test

---
+++ tests/graphics/awful_radial_gradient/main.c
@@ -0,0 +1,25 @@
+#include <recomp.h>
+#include <constants.h>
+#include <draws.h>
+
+int main()
+{
+	SWFAppContext app_context;
+	app_context.frame_funcs = frame_funcs;
+	app_context.width = FRAME_WIDTH;
+	app_context.height = FRAME_HEIGHT;
+	app_context.stage_to_ndc = stage_to_ndc;
+	
+	app_context.shape_data = (char*) shape_data;
+	app_context.shape_data_size = sizeof(shape_data);
+	app_context.transform_data = (char*) transform_data;
+	app_context.transform_data_size = sizeof(transform_data);
+	app_context.color_data = (char*) color_data;
+	app_context.color_data_size = sizeof(color_data);
+	app_context.gradmat_data = (char*) gradmat_data;
+	app_context.gradmat_data_size = sizeof(gradmat_data);
+	app_context.gradient_data = (char*) gradient_data;
+	app_context.gradient_data_size = sizeof(gradient_data);
+	
+	swfStart(&app_context);
+}
