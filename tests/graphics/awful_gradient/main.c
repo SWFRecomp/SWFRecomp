c51a6b6abdd5208888934db7e53d302cdd04171d
LittleCube <littlecubehax@gmail.com>
2025-11-27 22:45:22 -0500

    feed max string id to app context

--- tests/graphics/awful_gradient/main.c
+++ tests/graphics/awful_gradient/main.c
@@ -2,6 +2,8 @@
 #include <constants.h>
 #include <draws.h>
 
+#include <script_decls.h>
+
 int main()
 {
 	SWFAppContext app_context;
@@ -35,6 +37,8 @@ int main()
 	app_context.text_data_size = sizeof(text_data);
 	app_context.cxform_data = (char*) cxform_data;
 	app_context.cxform_data_size = sizeof(cxform_data);
+	
+	app_context.max_string_id = MAX_STRING_ID;
 	
 	swfStart(&app_context);
 }
